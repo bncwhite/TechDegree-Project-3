88d5b9affc834c234925a9cc0b384eb99d18b89a
Bradley White <codebybrad@icloud.com>
2017-09-18 17:39:41 -0500

    Initial Commit

---
+++ Bout Time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bout Time
+//
+//  Created by Bradley White on 9/18/17.
+//  Copyright © 2017 Bradley White. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
