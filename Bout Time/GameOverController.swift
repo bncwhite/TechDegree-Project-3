2181b35e8f66ab58e23f02fc3ebe889b4b5fcde6
Bradley White <codebybrad@icloud.com>
2017-09-18 19:44:06 -0500

    Initial UI Layout setup complete

---
+++ Bout Time/GameOverController.swift
@@ -0,0 +1,35 @@
+//
+//  GameOverController.swift
+//  Bout Time
+//
+//  Created by Bradley White on 9/18/17.
+//  Copyright © 2017 Bradley White. All rights reserved.
+//
+
+import UIKit
+
+class GameOverController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
